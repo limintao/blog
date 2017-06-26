3c7836a8a85af9131e85d57d69b77e9e2f3933b9
李民涛 <tc_lmh@tzyun.com>
2017-06-26 16:35:30 +0800

    继续改进！

--- public/javascript/index/index.js
+++ public/javascript/index/index.js
@@ -1,6 +1,17 @@
 document.body.onfocus = function(){
-    document.title = '这里是网站标题';
+    document.title = "首页";
 };
 document.body.onblur = function(){
     document.title = 'i miss you';
 };
+
+
+let time = new Date();
+
+console.log(time)
+
+$(function () {
+
+    console.log(parseInt($(".media-right").children("span").text()) );
+});
+
